409417dc217e71e92feb591fd76923d062f3cde8
sandhiyapragash <sandhiyapragash@gmail.com>
2023-12-07 12:23:20 +0530

    First commit

---
+++ src/Pages/LoginPage.java
@@ -0,0 +1,12 @@
+package Pages;
+
+public class LoginPage {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Login Page");
+	}
+
+	
+	
+
+}
---
+++ src/Tests/HomePageTest.java
@@ -0,0 +1,9 @@
+package Tests;
+
+public class HomePageTest {
+	
+	public void test1() {
+		System.out.println("test 1 -- Home Page");
+	}
+
+}
