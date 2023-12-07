0ddbb35ebbe6c06c6b9b633d48af3b99883a0e44
sandhiyapragash <sandhiyapragash@gmail.com>
2023-12-07 12:40:19 +0530

    Second Commit

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Login Page");
+		String us="admin";
+		String pwd="admin";
+		System.out.println("Logged In.....");
 	}
 
 	
--- src/Tests/HomePageTest.java
+++
@@ -1,9 +0,0 @@
-package Tests;
-
-public class HomePageTest {
-	
-	public void test1() {
-		System.out.println("test 1 -- Home Page");
-	}
-
-}
